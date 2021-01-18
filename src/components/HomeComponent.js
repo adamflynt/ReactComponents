b5aa14266231f210130bd68cc25b53ebb41272c6
Adam Flynt <adam.e.flynt@gmail.com>
2021-01-18 11:28:55 -0600

    React Animation Components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,39 +2,47 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
 
-function RenderCard({item, isLoading, errMess}) {
+
+function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
-        return(
+        return (
             <Loading />
         );
     }
     else if (errMess) {
-        return(
+        return (
             <h4>{errMess}</h4>
         );
     }
     else if (item) {
-        return(
-        <Card>
-            <CardImg src={baseUrl + item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card> 
+        return (
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
-        }
-        else
-        return(
+    }
+    else
+        return (
             <div></div>
         );
 }
 
 function Home(props) {
-    return(
+    return (
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
@@ -43,9 +51,9 @@ function Home(props) {
                         errMess={props.dishesErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                <RenderCard item={props.promotion}
-                    isLoading={props.promoLoading} 
-                    errMess={props.promoErrMess} />
+                    <RenderCard item={props.promotion}
+                        isLoading={props.promoLoading}
+                        errMess={props.promoErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader} />
